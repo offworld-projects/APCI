ac3b5f2db4d216928e5bbc2d8f8dde46e78f7c11
jwdinius <josephwdinius@gmail.com>
2022-08-23 18:07:06 -0700

    AAGSB-1207 add install script

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# See README for instructions, as well as 
+# Jira ticket - https://offworld-ai.atlassian.net/browse/AAGSB-1207
+# for more context
+
+# install linux kernel headers
+sudo apt install -y linux-headers-$(uname -r)
+
+# install libncurses-dev (needed if you wish to compile the apcilib CLI examples)
+sudo apt install -y libncurses-dev
+
+# compile 
+make
+
+# install/load
+sudo make install
+export PYTHONPATH="$PYTHONPATH:/usr/local/lib"
